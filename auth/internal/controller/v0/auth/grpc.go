f079af6758883c87340fbdca5d3bbdea078b119e
Zakhar <mix.cat@mail.ru>
2024-08-23 15:44:17 +0300

    ?

--- auth/internal/controller/v0/auth/grpc.go
+++ auth/internal/controller/v0/auth/grpc.go
@@ -1,3 +1,10 @@
+// порядок должены быть
+// validation
+// mapping
+// use-case call
+// errors handle
+// mapping
+
 package auth
 
 import (
@@ -8,8 +15,6 @@ import (
 	pb "yir/auth/api/v0/auth"
 	"yir/auth/internal/controller/usecases"
 	"yir/auth/internal/enity"
-
-	"go.uber.org/zap"
 )
 
 // Тут вроде можно просто server, но я не определился
@@ -17,16 +22,13 @@ type AuthServer struct {
 	pb.UnimplementedAuthServer
 
 	authUseCase usecases.Auth
-	logger      *zap.Logger
 }
 
 func NewAuthServer(
 	authUseCase usecases.Auth,
-	logger *zap.Logger,
 ) *AuthServer {
 	return &AuthServer{
 		authUseCase: authUseCase,
-		logger:      logger,
 	}
 }
 
@@ -39,10 +41,12 @@ func (s *AuthServer) Login(ctx context.Context, request *pb.LoginRequest) (*pb.L
 	tokenPair, err := s.authUseCase.Login(ctx, &domainRequest)
 	if err != nil {
 		switch {
+		case statusE, ok := status.FromError(err); ok:
 		case errors.Is(err, enity.ErrNotFound):
 			return nil, status.Error(codes.NotFound, err.Error())
 		case errors.Is(err, enity.ErrPassHashNotEqual):
 			return nil, status.Error(codes.InvalidArgument, err.Error())
+
 		}
 	}
 
@@ -50,12 +54,4 @@ func (s *AuthServer) Login(ctx context.Context, request *pb.LoginRequest) (*pb.L
 		AccessToken:  tokenPair.AccessToken,
 		RefreshToken: tokenPair.RefreshToken,
 	}, nil
-}
-
-func (s *AuthServer) Register(ctx context.Context, request *pb.RegisterRequest) (*pb.RegisterResponse, error) {
-	return nil, nil
-}
-
-func (s *AuthServer) TokenRefresh(ctx context.Context, request *pb.TokenRefreshRequest) (*pb.TokenRefreshResponse, error) {
-	return nil, nil
 }
