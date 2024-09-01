044c254b511d922172dd0fa435f6c921b46a6b04
eternalove4u <dadan.smirnoff@yandex.ru>
2024-09-01 19:46:12 +0300

    Add: config + cool parsing (env) + refactor config.go + go.mod/go.sum

---
+++ med/cmd/card/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"yir/med/internal/config"
+)
+
+const (
+	envDev  = "dev"
+	envProd = "prod"
+)
+
+const (
+	defaultCfgPath = "med/config/config.yaml"
+	shorthand      = " (shorthand)"
+)
+
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config", defaultCfgPath, "set config path")
+	flag.StringVar(&configPath, "c", defaultCfgPath, "set config path"+shorthand)
+}
+
+func main() {
+
+	// INIT ZAP LOGGER
+
+	flag.Parse()
+	cfg, err := config.NewConfig(configPath)
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err.Error())
+	}
+
+	log.Printf("config: %+v", cfg)
+}
