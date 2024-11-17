e45991db9894a49dbec38bdf530df2743db8b0ee
dexxhawk <Dsshnyakin@yandex.ru>
2024-11-17 17:48:09 +0300

    Add settings from env

--- ml_service/main.py
+++ ml_service/main.py
@@ -1,56 +1,49 @@
-from minio import Minio
-from minio.error import S3Error
-import time
-
 from concurrent import futures
+
 import grpc
-import sys
+from minio import Minio
 
-import ml_service.api.ml_api_pb2 as pb
 import ml_service.api.ml_api_pb2_grpc as pb_grpc
-
-
-
-import ml_service.internal.events.events as kafkaevents
-
-
-
-import ml_service.internal.s3.s3 as mys3
-
-import ml_service.internal.ml_model.segmentation as seg
-import ml_service.internal.ml_model.classification_efficientnet as cla
-
-import ml_service.internal.usecases.uzi.uzi as usecaseuzi
 import ml_service.internal.api.ml_controller as ctrl
+import ml_service.internal.events.events as kafkaevents
+import ml_service.internal.ml_model.classification_efficientnet as cla
+import ml_service.internal.ml_model.segmentation as seg
+import ml_service.internal.s3.s3 as mys3
+import ml_service.internal.usecases.uzi.uzi as usecaseuzi
+from ml_service.config.default import get_settings
+
 
 def run_server():
+    settings = get_settings()
 
     minio_client = Minio(
-        "localhost:9000",  # Например, "localhost:9000" или "minio.example.com:9000"
-        access_key="z1qTDuqQH233Di9sWyiV",
-        secret_key="OXgQ5UKoVvejYCEEpUb3xBZ8rvW4dwHNNO7O2b2L",
-        secure=False  # Установите True, если используете HTTPS
+        endpoint=settings.s3_endpoint,
+        access_key=settings.s3_access_key,
+        secret_key=settings.s3_secret_key.get_secret_value(),
+        secure=False,  # Установите True, если используете HTTPS
     )
-
-    bucket = "uzi"
+    bucket = settings.s3_bucket_name
 
     s3 = mys3.S3(minio_client, bucket)
 
-    segmdl = seg.SegmentationModel('cross')
-    claml = cla.EfficientNetModel('cross')
+    segmdl = seg.SegmentationModel(model_type=settings.segmentation_model_type)
+    claml = cla.EfficientNetModel(model_type=settings.classification_model_type)
 
     usecase = usecaseuzi.uziUseCase(segmdl, claml, s3)
 
     controller = ctrl.MlController(usecase)
     kafka = kafkaevents.EventsYo(usecase)
-    print("РАЗЪЕБНАЯ КАФКА ПОЕХАЛА")
+    print("Kafka started...")
     kafka.run()
 
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     pb_grpc.add_MLAPIServicer_to_server(controller, server)
-    server.add_insecure_port('[::]:50057')
+
+    server_port = str(settings.grpc_port)
+
+    server.add_insecure_port("[::]:" + server_port)
     server.start()  # Запускаем сервер
-    print("Server started on port 50057")
+    print(f"Server started on port {server_port}")
     server.wait_for_termination()
 
     # try:
