ca3687b3f5d38d194e5a94f70e38807b059ce2e4
wantbeasleep <mix.cat@mail.ru>
2025-01-19 19:12:55 +0300

    hotfix migration

--- uzi/db/migrations/00003_add_uzi_id_node_field.sql
+++ uzi/db/migrations/00003_add_uzi_id_node_field.sql
@@ -1,7 +1,18 @@
 -- +goose Up
 -- +goose StatementBegin
-ALTER TABLE node
-    ADD uzi_id uuid NOT NULL REFERENCES uzi (id);
+ALTER TABLE node ADD uzi_id uuid REFERENCES uzi (id);
+
+UPDATE node 
+SET uzi_id = subq.uzi_id
+FROM (
+    SELECT DISTINCT i.uzi_id, n.id
+    FROM image i
+    JOIN segment s ON s.image_id = i.id
+    JOIN node n ON s.node_id = n.id
+) as subq
+WHERE node.id = subq.id;
+
+ALTER TABLE node ALTER COLUMN uzi_id SET NOT NULL;
 
 COMMENT ON COLUMN node.uzi_id IS 'Идентификатор узи';
 -- +goose StatementEnd
